be090674886342a3bd586597a74b7df814da6c27
Jaehyeon1020 <kjhkk1020@naver.com>
2023-05-15 23:26:15 +0900

    fix: cors origin 수정

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def start_application():
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=["http://localhost:3000"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
